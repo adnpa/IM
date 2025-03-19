a3f21ef5f06f188fa09edad7508f4681e1b942de
hz <edj6796@gmail.com>
2025-03-19 23:28:41 +0800

    update

--- app/transfer/global/global.go
+++ app/transfer/global/global.go
@@ -1,6 +1,7 @@
 package global
 
 import (
+	"github.com/adnpa/IM/api/pb"
 	"github.com/adnpa/IM/app/user/config"
 	"github.com/adnpa/IM/pkg/common/mq/rabbitmq"
 	"github.com/adnpa/IM/pkg/common/storage"
@@ -9,6 +10,9 @@ import (
 var (
 	Producer     *rabbitmq.Producer
 	RedisPool    storage.Pool
+	PresenceCli  pb.PresenceClient
+	OffineCli    pb.OfflineClient
+	GroupCli     pb.GroupClient
 	ServerConfig config.ServerConfig
 	NacosConfig  config.NacosConfig
 )
--- app/transfer/service/consumer.go
+++ app/transfer/service/consumer.go
@@ -1,11 +1,18 @@
 package service
 
 import (
-	"log"
+	"context"
+	"encoding/json"
+	"fmt"
 
 	"github.com/adnpa/IM/api/pb"
+	"github.com/adnpa/IM/app/transfer/global"
+	"github.com/adnpa/IM/app/transfer/model"
+	"github.com/adnpa/IM/internal/utils"
 	"github.com/adnpa/IM/pkg/logger"
+	"github.com/jinzhu/copier"
 	amqp "github.com/rabbitmq/amqp091-go"
+	"go.uber.org/zap"
 )
 
 func NewConsumer(c *amqp.Connection) *Consumer {
@@ -20,15 +27,6 @@ type Consumer struct {
 	conn *amqp.Connection
 }
 
-// func TestSend() {
-// 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer conn.Close()
-// 	go NewConsumer(conn).Run()
-// }
-
 func (c *Consumer) Run() {
 	ch, err := c.conn.Channel()
 	if err != nil {
@@ -37,12 +35,12 @@ func (c *Consumer) Run() {
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
+		"im_message", // name
+		false,        // durable
+		false,        // delete when unused
+		false,        // exclusive
+		false,        // no-wait
+		nil,          // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
 
@@ -66,8 +64,83 @@ func (c *Consumer) Run() {
 	}
 }
 
-func (c *Consumer) handleMsg(msg []byte) error {
-	log.Printf("Received a message: %s", msg)
+func (c *Consumer) handleMsg(data []byte) error {
+	msg := &model.Message{}
+	err := json.Unmarshal(data, msg)
+	if err != nil {
+		return err
+	}
+	logger.Debug("recv msg", zap.Any("msg", msg))
+
+	switch msg.Cmd {
+	case model.TypSingle:
+		c.handleSingle(msg)
+	case model.TypGroup:
+		c.handleGroup(msg)
+	default:
+	}
+	return nil
+}
+
+func (c *Consumer) handleSingle(msg *model.Message) error {
+	if msg.To == msg.From {
+		return fmt.Errorf("send msg to self is not support")
+	}
+
+	// todo 序列号去重
+
+	// todo 分布式id服务
+	msg.Id = utils.NowMilliSecond()
+
+	pbMsg := &pb.ChatMsg{}
+	copier.Copy(pbMsg, msg)
+
+	resp, err := global.PresenceCli.IsOnline(context.Background(), &pb.IsOnlineReq{UserId: int32(msg.To)})
+	if err != nil {
+		return err
+	}
+
+	// 总是先入库 收到用户ack再把数据删掉
+	putMsgResp, err := global.OffineCli.PutMsg(context.Background(), &pb.PutMsgReq{UserId: int32(msg.To), Msg: pbMsg})
+	if err != nil || !putMsgResp.Succ {
+		return err
+	}
+
+	if resp.IsOnline {
+		_, err := global.PresenceCli.SendMsg(context.Background(), &pb.SendMsgReq{UserId: int32(msg.To), Msg: pbMsg})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (c *Consumer) handleGroup(msg *model.Message) error {
+	msg.Id = utils.NowMilliSecond()
+
+	pbMsg := &pb.ChatMsg{}
+	copier.Copy(pbMsg, msg)
+
+	resp, err := global.GroupCli.GetGroupMemberById(context.Background(), &pb.GetGroupMemberByIdReq{GroupId: msg.To})
+	if err != nil {
+		return err
+	}
+	// logger.Infof("send to all group members", "members", users)
+	// 对群里所有用户 复制一条消息到队列
+	for _, member := range resp.Members {
+		_, err := global.OffineCli.PutMsg(context.Background(), &pb.PutMsgReq{UserId: member.UserId, Msg: pbMsg})
+		if err != nil {
+			return err
+		}
+
+		preResp, err := global.PresenceCli.IsOnline(context.Background(), &pb.IsOnlineReq{UserId: member.UserId})
+		if err != nil {
+			return err
+		}
+		if preResp.IsOnline {
+			global.PresenceCli.SendMsg(context.Background(), &pb.SendMsgReq{Msg: pbMsg})
+		}
+	}
 	return nil
 }
 
